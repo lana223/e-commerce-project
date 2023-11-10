340412d52dc11ff1bac4b077cd731c9547f46696
lana223 <48509802+lana223@users.noreply.github.com>
2023-11-10 22:44:30 +0200

    change hero bg

--- src/components/HomeComponent/Hero.vue
+++ src/components/HomeComponent/Hero.vue
@@ -1,7 +1,7 @@
 
    <template>
   <v-parallax
-    src="https://img.freepik.com/premium-photo/online-shopping-concept-3d-rendering_651547-364.jpg?size=626&ext=jpg&ga=GA1.1.44546679.1699401600&semt=ais"
+    src="https://static.vecteezy.com/system/resources/previews/017/380/592/non_2x/three-dressed-mannequins-standing-in-a-store-casual-women-s-clothing-store-in-the-mall-boutique-mannequins-in-a-fashion-store-shopping-store-without-people-ukraine-kyiv-may-23-2022-free-photo.jpg"
   >
     <div class="d-flex flex-column fill-height justify-center align-center text-black">
       <h1 class="text-h1 font-weight-bold ">
