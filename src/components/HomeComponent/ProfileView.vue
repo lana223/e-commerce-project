537d2cd43c13015f7075bd66f4694819c08a0fc2
lana223 <48509802+lana223@users.noreply.github.com>
2023-11-11 14:32:15 +0200

    initial profile page

---
+++ src/components/HomeComponent/ProfileView.vue
@@ -0,0 +1,20 @@
+<template >
+   <v-container class="text-center " >
+       <v-row>
+           <v-col cols="3" class="mx-auto my-8">
+               <v-card  >
+                 <v-card-title > {{ Response.lastName }}</v-card-title>
+                 <v-card-subtitle>{{ Response.email }}</v-card-subtitle>
+              
+               </v-card>
+           </v-col>
+       </v-row>
+   </v-container>
+</template>
+
+<script setup>
+import {useResStore} from '@/store/LogIn.js'
+const LogIn = useResStore() ;
+let Response = LogIn.res
+
+</script>
